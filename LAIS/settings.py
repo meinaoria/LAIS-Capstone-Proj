d05d0d85cf07483131b2aeb2820c92690025382e
Eyob-t <etila051@uottawa.ca>
2019-11-29 00:29:49 -0500

    Translation started

--- LAIS/settings.py
+++ LAIS/settings.py
@@ -50,6 +50,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -115,13 +116,23 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
+
 LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = [
+    ('en','English'),
+    ('fr', 'French'),
+]
 
 TIME_ZONE = 'America/Toronto'
 
 USE_I18N = True
 
 USE_L10N = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
 #USE_TZ = True
 
@@ -130,6 +141,10 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
--- LAIS/urls.py
+++ LAIS/urls.py
@@ -19,10 +19,10 @@ from django.urls import path, include
 from users import views as user_views
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('status_board.urls')),
-   
-
 ]
+
